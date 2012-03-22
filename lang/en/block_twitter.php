80580779ee4bc48248949d003aa5837ec3f3b627
Chris Murad <cmurad@localhost.localdomain>
2012-03-22 11:30:30 -0500

    cleanup

--- lang/en/block_twitter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$string['pluginname'] = 'Twitter';
-$string['search_string'] = 'Twitter Username';
-$string['config_header'] = 'Twitter Block Config';
-$string['block_header'] = 'Block Header';
-$string['count'] = '# of Tweets';
-$string['default_twitter'] = 'USCedu';
-$string['teacher_role'] = 'editingteacher';
