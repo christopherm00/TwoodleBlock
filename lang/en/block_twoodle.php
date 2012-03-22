d9cb1853c3dc6605a221fcc33b46c19d5acf9284
slag02 <cmurad00@gmail.com>
2012-03-22 11:43:52 -0500

    Update lang/en/block_twoodle.php

--- lang/en/block_twoodle.php
+++ lang/en/block_twoodle.php
@@ -12,5 +12,5 @@ $string['search_string'] = 'Twitter Username';
 $string['config_header'] = 'Twitter Block Config';
 $string['block_header'] = 'Block Header';
 $string['count'] = '# of Tweets';
-$string['default_twitter'] = 'USCedu';
+$string['default_twitter'] = 'moodle';
 $string['teacher_role'] = 'editingteacher';
