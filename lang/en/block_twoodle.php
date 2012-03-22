abf938256625919361875f75f46e6604dfd1b1b1
Chris Murad <cmurad@localhost.localdomain>
2012-03-22 11:32:17 -0500

    cleanup

---
+++ lang/en/block_twoodle.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+* Created Mar 2011
+*
+* @author Matt Burica
+*
+*/
+
+$string['pluginname'] = 'Twoodle';
+$string['search_string'] = 'Twitter Username';
+$string['config_header'] = 'Twitter Block Config';
+$string['block_header'] = 'Block Header';
+$string['count'] = '# of Tweets';
+$string['default_twitter'] = 'USCedu';
+$string['teacher_role'] = 'editingteacher';
